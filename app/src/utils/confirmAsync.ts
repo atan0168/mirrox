150118f130b67ede8d311e83f2b3ccecebcbcf68
Seleneplus <1826853804@qq.com>
2025-09-28 16:00:43 +0800

    update confirmAsync to use English labels

--- app/src/utils/confirmAsync.ts
+++ app/src/utils/confirmAsync.ts
@@ -2,9 +2,9 @@ import { Alert } from 'react-native';
 
 export function confirmAsync(message: string): Promise<boolean> {
   return new Promise(resolve => {
-    Alert.alert('确认', message, [
-      { text: '否', onPress: () => resolve(false), style: 'cancel' },
-      { text: '是', onPress: () => resolve(true) },
+    Alert.alert('Confirm', message, [
+      { text: 'Cancel', onPress: () => resolve(false), style: 'cancel' },
+      { text: 'OK', onPress: () => resolve(true) },
     ]);
   });
 }
